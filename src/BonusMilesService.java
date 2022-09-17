0bf8da194a253c3aad98f65f7bdc3bd1b5249c6f
Mihail Nephedov <Nephedov93@gmail.com>
2022-09-17 19:50:35 +0300

    variable name changed

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
-    public int calculate (int a) {
-        int result = a / 20;
+    public int calculate(int mls) {
+        int result = mls / 20;
 
         return result;
     }
